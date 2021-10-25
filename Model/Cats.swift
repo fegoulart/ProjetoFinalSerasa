afe63a8847e5f086ca200fcfd91bfc51a76bf35e
Thay Mendes <thay489@gmail.com>
2021-10-24 23:02:43 -0300

    inicialixacao do projeto e criacao da struct correspondente ao JSON

---
+++ Model/Cats.swift
@@ -0,0 +1,59 @@
+//
+//  Cats.swift
+//  CatAPI
+//
+//  Created by Thayanne Viana on 21/10/21.
+//
+
+import Foundation
+class CatMapper{
+    // static func map(var catDTO) -> Cat{
+    
+}
+
+struct Cat: Codable{
+    var temperament        : Temperament?
+    var weigth             : String?
+    var id                 : String?
+    var name               : String?
+    var caf_url            : String?
+    var vetstreet_url      : String?
+    var vcahospitals_url   : String?
+    var origin             : String?
+    var country_code       : String?
+    var description        : String?
+    var life_span          : String?
+    var wikipedia_url      : String?
+    var reference_image_id : String?
+    var image              : String?
+    var indoor             : Bool?
+    var lap                : Bool?
+    var alt_names          : Bool?
+    var adaptability       : Bool?
+    var affection_level    : Bool?
+    var child_friendly     : Bool?
+    var dog_friendly       : Bool?
+    var energy_level       : Bool?
+    var grooming           : Bool?
+    var health_issues      : Bool?
+    var intelligence       : Bool?
+    var shedding_level     : Bool?
+    var social_needs       : Bool?
+    var vocalisation       : Bool?
+    var experimental       : Bool?
+    var hairless           : Bool?
+    var natural            : Bool?
+    var rare               : Bool?
+    var rex                : Bool?
+    var suppressed_tail    : Bool?
+    var short_legs         : Bool?
+    var hypoallergenic     : Bool?
+    
+     
+    
+    
+    
+}
+struct Temperament {
+    
+}
