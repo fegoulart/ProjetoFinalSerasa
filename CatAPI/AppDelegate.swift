85f914dd423562d81e9ca27b713273ddfb39af65
Fernando Luiz Goulart <fernando@leapi.com>
2021-10-30 17:05:16 -0300

    TabBar + FavoritesViewController

--- CatAPI/AppDelegate.swift
+++ CatAPI/AppDelegate.swift
@@ -14,15 +14,44 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        let viewController: UIViewController = HomeViewController( )
-        let navigationController =  UINavigationController(rootViewController: viewController)
         self.window = UIWindow(frame: UIScreen.main.bounds)
-        self.window?.rootViewController =  navigationController
+        self.window?.rootViewController =  setupTabBar()
         self.window?.makeKeyAndVisible()
 
         print("Tamanho: \(UIScreen.main.bounds)")
 
         return true
+    }
+
+    private func setupTabBar() -> UITabBarController {
+        let tabBarController = UITabBarController()
+        tabBarController.tabBar.backgroundColor = .systemGray
+        tabBarController.tabBar.tintColor = .white
+        tabBarController.tabBar.unselectedItemTintColor = .darkGray
+        tabBarController.viewControllers = setupViewControllers()
+        return tabBarController
+    }
+
+    private func setupViewControllers() -> [UIViewController] {
+        return [mainViewController(), favoritesViewController()]
+    }
+
+    private func mainViewController() -> UIViewController {
+        let viewController = ViewController()
+        let navigationController =  UINavigationController(rootViewController: viewController)
+        navigationController.tabBarItem = tabBarItem(title: "home", systemIcon: "house")
+        return navigationController
+    }
+
+    private func favoritesViewController() -> UIViewController {
+        let viewController = FavoritesViewController()
+        viewController.tabBarItem = tabBarItem(title: "favorites", systemIcon: "house")
+        return viewController
+    }
+
+    private func tabBarItem(title: String, systemIcon: String) -> UITabBarItem {
+        let tabBarImage = UIImage(systemName: systemIcon)
+        return UITabBarItem(title: title, image: tabBarImage, selectedImage: tabBarImage)
     }
 
 }
---
+++ CatAPI/FavoritesViewController.swift
@@ -0,0 +1,15 @@
+//
+//  FavoritesViewController.swift
+//  CatAPI
+//
+//  Created by Fernando Goulart on 30/10/21.
+//
+
+import UIKit
+
+class FavoritesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
