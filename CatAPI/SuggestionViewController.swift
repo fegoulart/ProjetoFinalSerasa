6b56b6442ad521f02c0cd2ad13fb143ec58bb644
Thay Mendes <thay489@gmail.com>
2021-11-01 12:59:56 -0300

    WIP: tela de sugestoes

--- CatAPI/SuggestionViewController.swift
+++ CatAPI/SuggestionViewController.swift
@@ -14,7 +14,6 @@ class SuggestionViewController: UIViewController {
     var suggestions: [Cats]?
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
 
     }
 
